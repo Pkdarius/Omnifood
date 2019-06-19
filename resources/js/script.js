26c09897d8b706fe4e72e8de283aa6c2d7a508b1
kycintern <50616882+kycintern@users.noreply.github.com>
2019-06-19 16:02:39 +0700

    Adding animations on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -48,4 +48,29 @@ $(document).ready(function () {
         }
       }
     });
+
+    /** Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+      $('.js--wp-1').addClass('animated fadeIn');
+    },{ 
+      offset: '500px;'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+      $('.js--wp-2').addClass('animated fadeInUp');
+    },{ 
+      offset: '500px;'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+      $('.js--wp-3').addClass('animated fadeIn');
+    },{ 
+      offset: '500px;'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+      $('.js--wp-4').addClass('animated pulse');
+    },{ 
+      offset: '500px;'
+    });
 });
